66675845037cf09f9d56d54b9c36a32f1cb86344
lian xu <1379960679@qq.com>
2024-07-16 17:21:10 +0800

    Update build.sh

--- job-template/job/datax/build.sh
+++ job-template/job/datax/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 #
-docker build -t registry.cn-hangzhou.aliyuncs.com/xula_docker/xula/datax:20230601 -f job/datax/Dockerfile .
-docker push registry.cn-hangzhou.aliyuncs.com/xula_docker/xula/datax:20230601
+docker build -t registry.cn-hangzhou.aliyuncs.com/xula_docker/datax:20230601 -f job/datax/Dockerfile .
+docker push registry.cn-hangzhou.aliyuncs.com/xula_docker/datax:20230601
 #
 #docker buildx build --platform linux/amd64 -t ccr.ccs.tencentyun.com/cube-studio/datax:20230501 -f job/datax/Dockerfile . --push
 
