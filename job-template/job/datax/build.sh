8e860dbe4ade75b7a65574425168b91888dc1ea4
lian xu <1379960679@qq.com>
2024-07-16 16:51:25 +0800

    Update build.sh

--- job-template/job/datax/build.sh
+++ job-template/job/datax/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 #
-docker build -t ccr.ccs.tencentyun.com/cube-studio/datax:20230601 -f job/datax/Dockerfile .
-docker push ccr.ccs.tencentyun.com/cube-studio/datax:20230601
+docker build -t 113.108.148.250:5013/ai/datax:20230601 -f job/datax/Dockerfile .
+docker push 113.108.148.250:5013/ai/datax:20230601
 #
 #docker buildx build --platform linux/amd64 -t ccr.ccs.tencentyun.com/cube-studio/datax:20230501 -f job/datax/Dockerfile . --push
 
